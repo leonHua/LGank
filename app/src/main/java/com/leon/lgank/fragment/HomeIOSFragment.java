b716a9ff3463575d931273f86d6789490a0c3b14
leonHua <open_leon@163.com>
2017-06-11 17:20:03 +0800

    界面美化

--- app/src/main/java/com/leon/lgank/fragment/HomeIOSFragment.java
+++ app/src/main/java/com/leon/lgank/fragment/HomeIOSFragment.java
@@ -1,8 +1,13 @@
 package com.leon.lgank.fragment;
 
+import android.content.Intent;
 import android.view.View;
 
+import com.blankj.utilcode.util.ToastUtils;
+import com.leon.lgank.adapter.HomeRecyclerviewAdapter;
 import com.leon.lgank.common.Constant;
+import com.leon.lgank.model.GankModel;
+import com.leon.lgank.ui.DetailActivity;
 
 /**
  * 作者：Leon
@@ -19,5 +24,21 @@ public class HomeIOSFragment extends BaseHomeFragment {
     @Override
     protected int getItemType() {
         return Constant.ITEM_TYPE_TEXT;
+    }
+    @Override
+    protected void initListener(HomeRecyclerviewAdapter mHomeRecyclerviewAdapter) {
+        mHomeRecyclerviewAdapter.addOnClickListener(new HomeRecyclerviewAdapter.OnBaseClickListener() {
+            @Override
+            public void onClick(GankModel.ResultsEntity entity) {
+                Intent intent = new Intent(mContext, DetailActivity.class);
+                intent.putExtra("url", entity.getUrl());
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCoverClick(GankModel.ResultsEntity entity) {
+                ToastUtils.showShortSafe("点击头像");
+            }
+        });
     }
 }
