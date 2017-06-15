72238b3de8d1f24212afe0b341d6fabb12b9577a
leonHua <open_leon@163.com>
2017-06-15 21:26:13 +0800

    完成个人中心界面，及部分功能

--- app/src/main/java/com/leon/lgank/fragment/MeFragment.java
+++ app/src/main/java/com/leon/lgank/fragment/MeFragment.java
@@ -1,13 +1,20 @@
 package com.leon.lgank.fragment;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.leon.lgank.R;
+import com.leon.lgank.ui.DetailActivity;
+import com.leon.lib.settingview.LSettingItem;
 
 /**
  * 作者：Leon
@@ -15,6 +22,11 @@ import com.leon.lgank.R;
  */
 public class MeFragment extends Fragment {
     public View mView;
+    public Activity mActivity;
+    private LSettingItem mSettingItemBlog;
+    private LSettingItem mSettingItemFollow;
+    private LSettingItem mSettingItemSave;
+    private LSettingItem mSettingItemVersion;
 
     @Nullable
     @Override
@@ -22,4 +34,47 @@ public class MeFragment extends Fragment {
         mView = inflater.inflate(R.layout.fragment_me, container, false);
         return mView;
     }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mActivity = (Activity) context;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        mSettingItemBlog = (LSettingItem) view.findViewById(R.id.item_blog);
+        mSettingItemFollow = (LSettingItem) view.findViewById(R.id.item_follow);
+        mSettingItemSave = (LSettingItem) view.findViewById(R.id.item_save);
+        mSettingItemVersion = (LSettingItem) view.findViewById(R.id.item_version);
+        mSettingItemBlog.setmOnLSettingItemClick(new LSettingItem.OnLSettingItemClick() {
+            @Override
+            public void click() {
+                Intent intent = new Intent(mActivity, DetailActivity.class);
+                intent.putExtra("url", "https://leonhua.github.io/");
+                startActivity(intent);
+            }
+        });
+        mSettingItemVersion.setmOnLSettingItemClick(new LSettingItem.OnLSettingItemClick() {
+            @Override
+            public void click() {
+                ToastUtils.showShortSafe("已经是最新版本啦~");
+            }
+        });
+        mSettingItemFollow.setmOnLSettingItemClick(new LSettingItem.OnLSettingItemClick() {
+            @Override
+            public void click() {
+                Intent intent = new Intent(mActivity, DetailActivity.class);
+                intent.putExtra("url", "https://github.com/leonHua");
+                startActivity(intent);
+            }
+        });
+        mSettingItemSave.setmOnLSettingItemClick(new LSettingItem.OnLSettingItemClick() {
+            @Override
+            public void click() {
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/leon/lgank/ui/MainActivity.java
+++ app/src/main/java/com/leon/lgank/ui/MainActivity.java
@@ -63,7 +63,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, SearchActivity.class));
         initToolbar();
         initView();
         initFragment();
